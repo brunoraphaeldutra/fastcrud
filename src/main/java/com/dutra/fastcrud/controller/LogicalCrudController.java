ffcf000ad9e32bd720ea67fb3b51974463bccf50
Bruno Dutra <brunordutra@gmail.com>
2021-01-12 17:50:13 -0300

    adjusted

--- src/main/java/com/dutra/fastcrud/controller/LogicalCrudController.java
+++ src/main/java/com/dutra/fastcrud/controller/LogicalCrudController.java
@@ -1,27 +1,23 @@
 package com.dutra.fastcrud.controller;
 
-import com.dutra.fastcrud.bases.CrudEntity;
 import com.dutra.fastcrud.bases.LogicalCrudEntity;
-import com.dutra.fastcrud.services.CrudService;
 import com.dutra.fastcrud.services.LogicalCrudService;
-import com.dutra.fastcrud.specifications.AbstractSpecification;
 import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public abstract class LogicalCrudController<T extends LogicalCrudEntity, ID> extends CrudController<T, ID> {
 
     public abstract LogicalCrudService<T, ID> getService();
 
-    @PutMapping("/{id}")
+    @PutMapping("/{id}/inactive")
     public ResponseEntity<ApiResponse> inactive(@RequestParam("id") Long id) throws ChangeSetPersister.NotFoundException {
         return ResponseEntity.ok(new ApiResponse(HttpStatus.OK, true, "Recovered.", getService().deactivated((ID) id)));
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/{id}/activate")
     public ResponseEntity<ApiResponse> active(@RequestParam("id") Long id) throws ChangeSetPersister.NotFoundException {
         return ResponseEntity.ok(new ApiResponse(HttpStatus.OK, true, "Recovered.", getService().activate((ID) id)));
     }
